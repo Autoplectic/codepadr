09e5d14ca137d94d601577b70f4cb759b91cf877
Ryan James <rgjames@ucdavis.edu>
2011-10-18 11:44:44 -0700

    update setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,26 @@
+#!/usr/bin/env python
 # encoding: utf-8
+
+import os
 from setuptools import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
     name='codepadr',
-    version='1.0',
-    url="https://github.com/autoplectic/codepadr",
-    license="LITL",
+    version='0.9',
     description="A tool for uploading to codepad.org",
+    long_description=read('README.rst'),
     author="Ryan James",
     author_email="rgjames@ucdavis.edu",
+    url="https://github.com/autoplectic/codepadr",
+    license="LITL",
     packages=['codepadr'],
     install_requires=['argparse', 'requests'],
     classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
         "Operating System :: OS Independent",
         "Programming Language :: Python",
     ],
